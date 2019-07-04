d91d878b783b17ebbdab0a37ce8fd4cac6500bd6
RODRIGO <cerdanrodrigo.utn@gmail.com>
2019-07-04 13:59:01 -0300

    ventana registro v1.5

--- proyectoCodigo2/vista/ventanaRegistro.py
+++ proyectoCodigo2/vista/ventanaRegistro.py
@@ -9,13 +9,15 @@ class Registro():
                 pass
             ventana=Tk()
             ventana.title("Registro")
-            ventana.geometry("700x590+350+0")
+            ventana.geometry("700x590+350+50")
             ventana.resizable(width=False,height=False)
             #-------------------imagen de fondo
             icono=PhotoImage(file="../imagenes/Registro.png")
             img=Label(ventana,image=icono).pack()
-            bt_register = Button(ventana, text="Registrar", fg="black", bg="red", command=validar, relief="flat", height=2,
-                          width=14,font=('Arial',11)).place(x=288, y=522)
+            bt_register = Button(ventana, text="Registrar", fg="black", bg="#FEE780", command=validar, relief="flat", height=2,
+                          width=13,font=('Arial',11)).place(x=215, y=521)
+            bt_cancelar = Button(ventana, text="Cancelar", fg="black", bg="#FEE780", command=validar, relief="flat", height=2,
+                          width=13,font=('Arial',11)).place(x=387, y=521)
             ventana.mainloop()
 
 x=Registro()
